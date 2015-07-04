e89c4daf831ee7fe63d53ef63c26c3588b9cb1a9
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-07-04 20:58:13 +0300

    Create page tast-list.html

---
+++ stickers/tasks/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+from tasks import views
+
+urlpatterns = [
+    url(r'^$', views.TaskListView.as_view(), name = 'tasks-list'),
+    #url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name = 'course'),
+    #Урлы для редактирование курса
+    #url(r'^add/$', views.CourseCreateView.as_view(), name = 'create-course'),
+    #url(r'^edit/(?P<pk>\d+)/$', views.CourseUpdateView.as_view(), name = 'edit-course'),
+    #url(r'^remove/(?P<pk>\d+)/$', views.CourseDeleteView.as_view(), name = 'remove-course'),
+    #Урлы для редактирования урока
+#    url(r'^(?P<pk>\d+)/add_lesson/$', views.create_lesson, name = 'create-lesson'),
+#    url(r'^(?P<pk>\d+)/edit_lesson/$', views.edit_lesson, name = 'edit-lesson'),
+#    url(r'^(?P<pk>\d+)/remove_lesson/$', views.remove_lesson, name = 'remove-lesson'),
+]
--- stickers/tasks/views.py
+++ stickers/tasks/views.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from tasks.models import Task
+
+
+# Create task list
+class TaskListView(ListView):
+    model = Task
+    #paginate_by = 8
+    def get_context_data(self, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        tasks = Task.objects.all()
+        context['tasks'] = tasks
+        #context['username'] = auth.get_user(self.request).username
+        return context
