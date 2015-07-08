a33a747e3e934f5542232e2247be76d967c8c2fd
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-07-08 19:20:06 +0300

    Some customize admin side of mr. Pencil

--- stickers/tasks/admin.py
+++ stickers/tasks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from tasks.models import Task, TaskItem
+
 
 
 class TaskItemInLine(admin.TabularInline):
@@ -8,11 +10,22 @@ class TaskItemInLine(admin.TabularInline):
     #field = ['theme', 'discription', 'number']
     item_display = ['discription', 'status']
 
+
 class TaskAdmin(admin.ModelAdmin):
-    #search_fields = ['name']
-    list_display = ['title', 'theme']
+    search_fields = ['user', 'title']
+    list_display = ['user', 'theme', 'title', 'time_public', 'impotent', 'fire']
+    list_filter = ['user', 'impotent', 'theme', 'time_public', 'time_finish',]
     inlines = [TaskItemInLine]
+    list_per_page = 20
+
+class TaskItemAdmin(admin.ModelAdmin):
+    list_display = ['commontask', 'discription', 'status']
+    search_fields = ['commontask', 'discription']
+    list_filter = ['status']
+
 
 
 admin.site.register(Task, TaskAdmin)
-admin.site.register(TaskItem)
+admin.site.register(TaskItem, TaskItemAdmin)
+#admin.site.register(User, UserAdmin)
+
