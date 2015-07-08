dadf0bb7c6d92b0b9dd5efa93b7f0f676d3f0aa0
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-07-08 22:32:48 +0300

    Add templates 404 and 500 errors

--- stickers/static/JS/script.js
+++ stickers/static/JS/script.js
@@ -9,9 +9,13 @@ $(document).ready(function(){
     });
 
     //Фича что-бы выезжало меню сортировки
-    $('.filter').fadeOut('slow');
+    //$('.filter').fadeOut('slow');
+    $('.filter').hide();
     $('.slidet').click(function(event) {
         $('.filter').slideToggle('slow');
         console.log('Привет');
     });
+    //Медленное появление завлекалки на стартовой странице
+    $('.custom-div').hide();
+    $('.custom-div').fadeTo(6000, 1.0);
 });
