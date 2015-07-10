ef35c29b8ef6e862aafb0ae4220dda404560d0bc
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-07-10 10:40:07 +0300

    Add simple test for Task and TaskItem

--- stickers/tasks/tests.py
+++ stickers/tasks/tests.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from tasks.models import Task, TaskItem
+
+
+class TaskTest(TestCase):
+    def test_desktop(self):
+        client = Client()
+        #Проверка на случай, когда ни стикер
+        response = client.get(reverse('tasks:tasks-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Пока нет никаких напоминалок :-(')
+        #Создаем пользователя
+        user = User.objects.create(username="Codebakery", email='nikolay.borovenskiy@gmail.com')
+        #Создаем два стикера
+        sticker1 = Task.objects.create(title='Reminder 1', theme='работа', user=user, impotent=True)
+        #Создаем 6 новых дел
+        for i in xrange(6):
+            nem_Item = TaskItem.objects.create(discription=u'Task {}'.format(i+1),
+                                               commontask=sticker1,
+                                               status=False,
+                                               )
+
+        #Проверка, что страница отвечает и стеке выводится правильно
+        response = client.get(reverse('tasks:tasks-list'))
+        self.assertEqual(response.status_code, 200)
+        for sticker in user.task_set.all():
+            self.assertContains(response, sticker.title)
+            self.assertContains(response, sticker.theme)
+            #Проверка, что все уроки курса выводятся
+            for item in sticker.taskitem_set.all()[:5]:
+                self.assertContains(response, item.discription)
